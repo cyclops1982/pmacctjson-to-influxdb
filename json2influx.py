1c3a3fed667867542a6155b42f1c84ad5ed2cbf8
Ruben d'Arco <cyclops@prof-x.net>
2022-04-16 19:26:41 +0100

    Write some real data.
    Next up - make all of the stuff variable so we can override with ENV vars.

--- json2influx.py
+++ json2influx.py
@@ -3,7 +3,7 @@
 import json
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def readapitoken():
@@ -23,9 +23,9 @@ point_settings.add_default_tag("hostname", "pyro.lnd.prof-x.net")
 write_api = client.write_api(write_options=SYNCHRONOUS,
                              point_settings=point_settings)
 
-inputfile = "./example.json"
-
+# File contains JSON lines like so:
 # {'event_type': 'purge', 'etype': '800', 'ip_src': '188.39.246.174', 'ip_dst': '85.214.51.245', 'ip_proto': 'tcp', 'stamp_inserted': '2022-04-16 14:25:00', 'stamp_updated': '2022-04-16 14:30:01', 'packets': 9, 'bytes': 6034}
+inputfile = "./example.json"
 
 tags = ['etype', 'ip_src', 'ip_dst', 'ip_proto']
 fields = ['bytes', 'packets', 'flows']
@@ -33,22 +33,13 @@ fields = ['bytes', 'packets', 'flows']
 with open(inputfile) as f:
     for line in f:
         jsonline = json.loads(line)
-        datetime_object = datetime.strptime(jsonline['stamp_updated'],
-                                            '%Y-%m-%d %H:%M:%S')
+        datetime_object = datetime.strptime(
+            jsonline['stamp_updated'],
+            '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
         p = Point("pmacct_traffic")
         for tag in tags:
             p.tag(tag, jsonline[tag])
         for field in fields:
-            p.field(fields, jsonline[fields])
+            p.field(field, jsonline[field])
         p.time(datetime_object)
-        print(p)
         write_api.write(bucket=bucket, record=p)
-
-## using Table structure
-# query_api = client.query_api()
-# tables = query_api.query('from(bucket:"pmacct") |> range(start: -10m)')
-
-#for table in tables:
-#    print(table)
-#    for row in table.records:
-#        print(row.values)
