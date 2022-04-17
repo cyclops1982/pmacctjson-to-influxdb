4a15a75ee19194c88501db08ec65723f131732e5
Ruben d'Arco <cyclops@prof-x.net>
2022-04-17 07:09:56 +0100

    Working version + some comments

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Simple script that reads PMACCT's JSON format and pushes it to influxdb
 
-
 Example JSON:
+
 ```json
 {"event_type": "purge", "etype": "800", "ip_src": "188.39.246.174", "ip_dst": "88.99.84.253", "ip_proto": "tcp", "stamp_inserted": "2022-04-16 13:30:00", "stamp_updated": "2022-04-16 13:35:01", "packets": 18, "bytes": 11882}
 {"event_type": "purge", "etype": "800", "ip_src": "188.39.246.174", "ip_dst": "198.51.45.8", "ip_proto": "udp", "stamp_inserted": "2022-04-16 13:30:00", "stamp_updated": "2022-04-16 13:35:01", "packets": 1, "bytes": 67}
@@ -11,9 +11,11 @@ Example JSON:
 
 ```
 
+## Source/inspiration
+
+`https://blog.fhrnet.eu/2019/10/15/retaking-control-of-your-network-part-2/` is a great place to start.
 
 ## Python environment
 
 We did a `python3.9 -v venv env` in the root of this repository and added `env` to `.gitignore`.
 Requirements are in `requirements.txt`
-
--- json2influx.py
+++ json2influx.py
@@ -2,44 +2,48 @@
 
 import json
 from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
+from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings, WriteOptions
 from datetime import datetime, timezone
 
 
 def readapitoken():
-    data = "FAIED TO READ TOKEN"
+    data = "FAILED TO READ TOKEN"
     with open('influxapi.token', 'r') as file:
         data = file.read().replace('\n', '')
     return data
 
 
 bucket = "pmacct"
-client = InfluxDBClient(url="http://jubilee.lnd.prof-x.net:8086",
-                        token=readapitoken(),
-                        org="Prof-X")
-
-point_settings = PointSettings()
-point_settings.add_default_tag("hostname", "pyro.lnd.prof-x.net")
-write_api = client.write_api(write_options=SYNCHRONOUS,
-                             point_settings=point_settings)
-
-# File contains JSON lines like so:
-# {'event_type': 'purge', 'etype': '800', 'ip_src': '188.39.246.174', 'ip_dst': '85.214.51.245', 'ip_proto': 'tcp', 'stamp_inserted': '2022-04-16 14:25:00', 'stamp_updated': '2022-04-16 14:30:01', 'packets': 9, 'bytes': 6034}
 inputfile = "./example.json"
+with InfluxDBClient(url="http://jubilee.lnd.prof-x.net:8086",
+                    token=readapitoken(),
+                    org="Prof-X") as client:
+    point_settings = PointSettings()
+    point_settings.add_default_tag("hostname", "pyro.lnd.prof-x.net")
+    with client.write_api(point_settings=point_settings) as write_api:
+        tags = ['etype', 'as_dst', 'as_src', 'ip_proto', 'ip_dst', 'ip_src']
+        fields = ['bytes']
 
-tags = ['etype', 'ip_src', 'ip_dst', 'ip_proto']
-fields = ['bytes', 'packets', 'flows']
+        with open(inputfile) as f:
+            for line in f:
+                jsonline = json.loads(line)
+                datetime_object = datetime.strptime(
+                    jsonline['stamp_inserted'],
+                    '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+                p = Point("pmacct_traffic")
+                for tag in tags:
+                    p.tag(tag, str(jsonline[tag]))
+                for field in fields:
+                    p.field(field, int(jsonline[field]))
+                p.time(datetime_object)
+                write_api.write(bucket=bucket, record=p, time_precision='s')
+    client.close()
 
-with open(inputfile) as f:
-    for line in f:
-        jsonline = json.loads(line)
-        datetime_object = datetime.strptime(
-            jsonline['stamp_updated'],
-            '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
-        p = Point("pmacct_traffic")
-        for tag in tags:
-            p.tag(tag, jsonline[tag])
-        for field in fields:
-            p.field(field, jsonline[field])
-        p.time(datetime_object)
-        write_api.write(bucket=bucket, record=p)
+## using Table structure
+# query_api = client.query_api()
+# tables = query_api.query('from(bucket:"pmacct") |> range(start: -10m)')
+
+#for table in tables:
+#    print(table)
+#    for row in table.records:
+#        print(row.values)
